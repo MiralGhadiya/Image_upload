c2d56dada694f265f7665762bb3c7046c4c3040f
MiralGhadiya <miral.g.evenmore@gmail.com>
2024-01-29 19:16:18 +0530

    changes

--- imageuploadproject/settings.py
+++ imageuploadproject/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'imageuploadproject.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "ImageUpload",
-        "USER": "postgres",
-        "PASSWORD": "admin",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
